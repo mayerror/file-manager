c3e66054ed2363eb5d7d6bf72ceb3731e018d73e
Sergey Masiuk <mayerror@gmail.com>
2024-02-05 00:08:14 +0300

    feat: implement greetings with username

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import fs from "node:fs";
 
-console.log(process.env);
+// console.log(process.argv);
+// console.log(process.env);
+
+process.argv.forEach((arg) => {
+  if (arg.includes("--username=")) {
+    const username = arg.split("=")[1];
+    process.stdout.write(`Welcome to the File Manager, ${username}!\n`);
+  }
+  // process.stdout.write(`${arg}\n`);
+});
