8e10dd5d0d5ff4428d73a5e47693e8b5c4817c61
Sergey Masiuk <mayerror@gmail.com>
2024-02-06 21:42:28 +0300

    fix: fix createfile error handle

--- src/modules/createFile.js
+++ src/modules/createFile.js
@@ -3,16 +3,16 @@ import fs from "node:fs";
 
 async function createFile(fileName) {
   const promise = await new Promise((res, rej) => {
-    try {
-      const path = join(globalThis.currentDir, fileName);
-      const writeStream = fs.createWriteStream(path, { flags: "ax" });
-      writeStream.end();
-      writeStream.on("close", () => {
-        res();
-      });
-    } catch (error) {
+    const path = join(globalThis.currentDir, fileName);
+    const writeStream = fs.createWriteStream(path, { flags: "ax" });
+
+    writeStream.end();
+    writeStream.on("close", () => {
+      res();
+    });
+    writeStream.on("error", () => {
       rej();
-    }
+    });
   });
   return promise;
 }
